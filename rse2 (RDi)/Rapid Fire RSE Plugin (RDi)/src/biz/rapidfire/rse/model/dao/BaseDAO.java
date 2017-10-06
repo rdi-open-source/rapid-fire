207b5f88cdfc8416d3c713b6f0e5a6854f7035cc
tools400 <thomas.raddatz@tools400.de>
2017-10-06 17:29:54 +0000

    Created AbstractBaseDAO to centralize code.

--- rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/model/dao/BaseDAO.java
+++ rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/model/dao/BaseDAO.java
@@ -1,18 +1,15 @@
 package biz.rapidfire.rse.model.dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import biz.rapidfire.core.RapidFireCorePlugin;
+import biz.rapidfire.core.model.dao.AbstractBaseDAO;
 import biz.rapidfire.core.model.dao.IBaseDAO;
 import biz.rapidfire.rse.Messages;
 
 import com.ibm.as400.access.AS400;
 import com.ibm.etools.iseries.subsystems.qsys.api.IBMiConnection;
 
-public class BaseDAO implements IBaseDAO {
+public class BaseDAO extends AbstractBaseDAO implements IBaseDAO {
 
     private static final String BOOLEAN_Y = "Y"; //$NON-NLS-1$
     private static final String BOOLEAN_N = "N"; //$NON-NLS-1$
@@ -51,69 +48,11 @@ public class BaseDAO implements IBaseDAO {
         connection.setAutoCommit(false);
     }
 
-    public PreparedStatement prepareStatement(String sql) throws SQLException {
-        return getConnection().prepareStatement(sql);
-    }
-
-    public void destroy() {
-
-        if (connection != null) {
-            try {
-                if (!connection.isClosed()) {
-                    connection.close();
-                    connection = null;
-                }
-            } catch (Throwable e) {
-                RapidFireCorePlugin.logError("*** Could not destroy connection ***", e); //$NON-NLS-1$
-            }
-        }
-    }
-
-    public void destroy(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) connection.close();
-    }
-
-    public void destroy(ResultSet resultSet) throws Exception {
-        if (resultSet != null) resultSet.close();
-    }
-
-    public void destroy(PreparedStatement preparedStatement) throws Exception {
-        if (preparedStatement != null) preparedStatement.close();
-    }
-
-    public void rollback(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            if (connection.getAutoCommit() == false) connection.rollback();
-        }
-    }
-
-    public void commit(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            if (connection.getAutoCommit() == false) connection.commit();
-        }
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
     public String getConnectionName() {
         return ibmiConnection.getConnectionName();
     }
 
     public AS400 getSystem() throws Exception {
         return ibmiConnection.getAS400ToolboxObject();
-    }
-
-    public boolean convertYesNo(String yesNoValue) {
-
-        boolean booleanValue;
-        if (BOOLEAN_YES.equals(yesNoValue) || BOOLEAN_Y.equals(yesNoValue)) {
-            booleanValue = true;
-        } else {
-            booleanValue = false;
-        }
-
-        return booleanValue;
     }
 }
