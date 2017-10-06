371578911be53b7e489102991f1fe8aaf86ab6ea
tools400 <thomas.raddatz@tools400.de>
2017-10-06 15:16:43 +0000

    Required changes for the WDSCi version of the plug-in.

--- eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/handlers/EndJobHandler.java
+++ eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/handlers/EndJobHandler.java
@@ -8,57 +8,22 @@
 
 package biz.rapidfire.core.handlers;
 
-import java.util.Iterator;
-
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
 
-import biz.rapidfire.core.RapidFireCorePlugin;
-import biz.rapidfire.core.model.IRapidFireJobResource;
-import biz.rapidfire.core.subsystem.IRapidFireSubSystem;
+import biz.rapidfire.rsebase.model.IRapidFireResource;
 
-public class EndJobHandler extends AbstractHandler implements IHandler {
+public class EndJobHandler extends AbstractJobHandler implements IHandler {
 
     public EndJobHandler() {
         super();
     }
 
-    /*
-     * (non-Javadoc)
-     * @see
-     * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
-     * ExecutionEvent)
-     */
-    public Object execute(ExecutionEvent event) throws ExecutionException {
+    protected Object executeWithResource(IRapidFireResource job) throws ExecutionException {
 
-        try {
+        System.out.println("Ending Rapid Fire job ... " + job.getName());
 
-            ISelection selection = HandlerUtil.getCurrentSelection(event);
-            if (selection instanceof IStructuredSelection) {
-                IStructuredSelection structuredSelection = (IStructuredSelection)selection;
-                Iterator<IRapidFireJobResource> iterator = structuredSelection.iterator();
-                while (iterator.hasNext()) {
-                    IRapidFireJobResource job = iterator.next();
-                    IRapidFireSubSystem subSystem = job.getParentSubSystem();
-
-                    System.out.println("Ending Rapid Fire job ... " + job.getName() + " (" + subSystem.getClass() + ")");
-                }
-            }
-
-        } catch (Exception e) {
-            RapidFireCorePlugin.logError("*** Error in EndJobHandler. ***", e); //$NON-NLS-1$
-        }
         return null;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return super.isEnabled();
     }
 
 }
