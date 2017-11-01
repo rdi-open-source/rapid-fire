f7ccd4fe9b81ac3a2b90be7b05cc9d33c5f9d3fd
tools400 <thomas.raddatz@tools400.de>
2017-11-01 14:10:37 +0000

    Changed copyright notice.

--- rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/subsystem/RapidFireSubSystemConfigurationAdapter.java
+++ rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/subsystem/RapidFireSubSystemConfigurationAdapter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012-2015 iSphere Project Owners
+ * Copyright (c) 2017-2017 Rapid Fire Project Team
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
- *     iSphere Project Owners - Maintenance and enhancements
+ *     Rapid Fire Project Team - Maintenance and enhancements
  *******************************************************************************/
 
 package biz.rapidfire.rse.subsystem;
