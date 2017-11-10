8d951edaeac1ca128f106a974a7f8f373a707874
tools400 <thomas.raddatz@tools400.de>
2017-11-10 10:22:30 +0000

    Added missing successFullyLoaded() to getFileCopyStatus().

--- rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/subsystem/RapidFireSubSystem.java
+++ rse2 (RDi)/Rapid Fire RSE Plugin (RDi)/src/biz/rapidfire/rse/subsystem/RapidFireSubSystem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017-2017 Rapid Fire Project Owners
+ * Copyright (c) 2017-2017 Rapid Fire Project Team
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -151,6 +151,10 @@ public class RapidFireSubSystem extends SubSystem implements IISeriesSubSystem, 
     }
 
     public IFileCopyStatus[] getFileCopyStatus(String library, String job, Shell shell) throws Exception {
+
+        if (!successFullyLoaded()) {
+            return new IFileCopyStatus[0];
+        }
 
         FileCopyStatusDAO dao = new FileCopyStatusDAO(getHostAliasName());
         List<IFileCopyStatus> fileCopyStatuses = dao.load(library, job, shell);
