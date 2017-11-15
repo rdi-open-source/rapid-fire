22538af16cd41ed9d4d975b49c31889586398d20
tools400 <thomas.raddatz@tools400.de>
2017-11-15 18:08:39 +0000

    Moved subsystem resource classes to core plug-in. (part 2 of 2)

---
+++ eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/subsystem/resources/RapidFireJobResource.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2017-2017 Rapid Fire Project Team
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package biz.rapidfire.core.subsystem.resources;
+
+import org.eclipse.rse.core.subsystems.AbstractResource;
+import org.eclipse.rse.core.subsystems.ISubSystem;
+
+import biz.rapidfire.core.model.IRapidFireJobResource;
+import biz.rapidfire.core.model.JobName;
+import biz.rapidfire.core.model.Phase;
+import biz.rapidfire.core.model.Status;
+import biz.rapidfire.core.subsystem.IRapidFireSubSystem;
+
+public class RapidFireJobResource extends AbstractResource implements IRapidFireJobResource, Comparable<IRapidFireJobResource> {
+
+    private String dataLibrary;
+    private String job;
+    private String description;
+    private boolean doCreateEnvironment;
+    private String jobQueueName;
+    private String jobQueueLibrary;
+    private Status status;
+    private Phase phase;
+    private boolean isError;
+    private String errorText;
+    private JobName batchJob;
+    private boolean isStopApplyChanges;
+    private String cmoneFormNumber;
+
+    public RapidFireJobResource(String dataLibrary, String job) {
+
+        this.dataLibrary = dataLibrary;
+        this.job = job;
+    }
+
+    /*
+     * IRapidFireResource methods
+     */
+
+    public String getDataLibrary() {
+        return dataLibrary;
+    }
+
+    public IRapidFireSubSystem getParentSubSystem() {
+        return (IRapidFireSubSystem)super.getSubSystem();
+    }
+
+    public void setParentSubSystem(IRapidFireSubSystem subSystem) {
+        super.setSubSystem((ISubSystem)subSystem);
+    }
+
+    /*
+     * IRapidFireJobResource methods
+     */
+
+    public String getName() {
+        return job;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDoCreateEnvironment() {
+        return doCreateEnvironment;
+    }
+
+    public void setDoCreateEnvironment(boolean doCreateEnvironment) {
+        this.doCreateEnvironment = doCreateEnvironment;
+    }
+
+    public String getJobQueueName() {
+        return jobQueueName;
+    }
+
+    public void setJobQueueName(String jobQueueName) {
+        this.jobQueueName = jobQueueName;
+    }
+
+    public String getJobQueueLibrary() {
+        return jobQueueLibrary;
+    }
+
+    public void setJobQueueLibrary(String jobQueueLibrary) {
+        this.jobQueueLibrary = jobQueueLibrary;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Phase getPhase() {
+        return phase;
+    }
+
+    public void setPhase(Phase phase) {
+        this.phase = phase;
+    }
+
+    public boolean isError() {
+        return isError;
+    }
+
+    public void setError(boolean isError) {
+        this.isError = isError;
+    }
+
+    public String getErrorText() {
+        return errorText;
+    }
+
+    public void setErrorText(String errorText) {
+        this.errorText = errorText;
+    }
+
+    public boolean isStopApplyChanges() {
+        return isStopApplyChanges;
+    }
+
+    public void setStopApplyChanges(boolean isStopApplyChanges) {
+        this.isStopApplyChanges = isStopApplyChanges;
+    }
+
+    public String getCmoneFormNumber() {
+        return cmoneFormNumber;
+    }
+
+    public void setCmoneFormNumber(String cmoneFormNumber) {
+        this.cmoneFormNumber = cmoneFormNumber;
+    }
+
+    public void setBatchJob(JobName job) {
+        this.batchJob = job;
+    }
+
+    public JobName getBatchJob() {
+        return batchJob;
+    }
+
+    public int compareTo(IRapidFireJobResource resource) {
+
+        if (resource == null) {
+            return 1;
+        }
+
+        int result = resource.getDataLibrary().compareTo(getDataLibrary());
+        if (result != 0) {
+            return result;
+        }
+
+        return getName().compareTo(resource.getName());
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+}
---
+++ eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/subsystem/resources/RapidFireLibraryResource.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2017-2017 Rapid Fire Project Team
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package biz.rapidfire.core.subsystem.resources;
+
+import org.eclipse.rse.core.subsystems.AbstractResource;
+import org.eclipse.rse.core.subsystems.ISubSystem;
+
+import biz.rapidfire.core.model.IRapidFireLibraryResource;
+import biz.rapidfire.core.subsystem.IRapidFireSubSystem;
+
+public class RapidFireLibraryResource extends AbstractResource implements IRapidFireLibraryResource, Comparable<IRapidFireLibraryResource> {
+
+    private String dataLibrary;
+    private String job;
+    private String library;
+    private String shadowLibrary;
+
+    public RapidFireLibraryResource(String dataLibrary, String job, String library) {
+
+        this.dataLibrary = dataLibrary;
+        this.job = job;
+        this.library = library;
+    }
+
+    /*
+     * IRapidFireResource methods
+     */
+
+    public String getDataLibrary() {
+        return dataLibrary;
+    }
+
+    public IRapidFireSubSystem getParentSubSystem() {
+        return (IRapidFireSubSystem)super.getSubSystem();
+    }
+
+    public void setParentSubSystem(IRapidFireSubSystem subSystem) {
+        super.setSubSystem((ISubSystem)subSystem);
+    }
+
+    /*
+     * IRapidFireLibraryResource methods
+     */
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getLibrary() {
+        return library;
+    }
+
+    public String getShadowLibrary() {
+        return shadowLibrary;
+    }
+
+    public void setShadowLibrary(String shadowLibrary) {
+        this.shadowLibrary = shadowLibrary;
+    }
+
+    public int compareTo(IRapidFireLibraryResource resource) {
+
+        if (resource == null) {
+            return 1;
+        }
+
+        int result = resource.getDataLibrary().compareTo(getDataLibrary());
+        if (result != 0) {
+            return result;
+        }
+
+        result = resource.getJob().compareTo(getJob());
+        if (result != 0) {
+            return result;
+        }
+
+        return getLibrary().compareTo(resource.getLibrary());
+    }
+
+    @Override
+    public String toString() {
+        return getLibrary();
+    }
+
+}
