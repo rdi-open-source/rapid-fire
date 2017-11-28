2baf74a9ebb91d86ccaf7dc0814bb634ec127142
tools400 <thomas.raddatz@tools400.de>
2017-11-28 17:26:31 +0000

    Added activity scheduler maintenance action.

---
+++ rse1 (WDSCi)/Rapid Fire RSE Plugin (WDSCi)/src/biz/rapidfire/rse/subsystem/actions/activity/ChangeActivitiesAction.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2017-2017 Rapid Fire Project Team
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package biz.rapidfire.rse.subsystem.actions.activity;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import biz.rapidfire.core.handlers.activity.ChangeActivitiesHandler;
+import biz.rapidfire.rse.subsystem.actions.AbstractResourceAction;
+
+public class ChangeActivitiesAction extends AbstractResourceAction {
+
+    private ChangeActivitiesHandler handler = new ChangeActivitiesHandler();
+
+    @Override
+    public void execute(ExecutionEvent event) throws ExecutionException {
+
+        handler.execute(event);
+    }
+
+}
