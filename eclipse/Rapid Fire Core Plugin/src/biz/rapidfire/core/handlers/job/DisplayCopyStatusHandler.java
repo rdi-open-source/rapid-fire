bee974bea9cdde2b0ef30573f4722a3d5628a4a3
tools400 <thomas.raddatz@tools400.de>
2017-12-07 14:27:38 +0000

    Changed DisplayCopyStatusHandler to inherit from AbstractJobMaintenanceHandler as the other handlers do.

--- eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/handlers/job/DisplayCopyStatusHandler.java
+++ eclipse/Rapid Fire Core Plugin/src/biz/rapidfire/core/handlers/job/DisplayCopyStatusHandler.java
@@ -8,66 +8,40 @@
 
 package biz.rapidfire.core.handlers.job;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 import biz.rapidfire.core.Messages;
 import biz.rapidfire.core.RapidFireCorePlugin;
-import biz.rapidfire.core.handlers.AbstractResourceHandler;
-import biz.rapidfire.core.handlers.shared.IMaintenanceHandler;
 import biz.rapidfire.core.helpers.ExceptionHelper;
 import biz.rapidfire.core.model.IRapidFireJobResource;
-import biz.rapidfire.core.model.IRapidFireResource;
 import biz.rapidfire.core.model.maintenance.MaintenanceMode;
+import biz.rapidfire.core.model.maintenance.job.shared.JobAction;
 import biz.rapidfire.core.view.FileCopyStatusView;
 
-public class DisplayCopyStatusHandler extends AbstractResourceHandler implements IHandler, IMaintenanceHandler {
+public class DisplayCopyStatusHandler extends AbstractJobMaintenanceHandler implements IHandler {
 
     public DisplayCopyStatusHandler() {
-        super(MaintenanceMode.DISPLAY);
-    }
-
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-
-        ISelection selection = getCurrentSelection(event);
-
-        return executeWithSelection(selection);
-    }
-
-    public void executeWDSCi(ExecutionEvent event) throws ExecutionException {
-        execute(event);
+        super(MaintenanceMode.DISPLAY, JobAction.DSPSTS);
     }
 
     @Override
-    protected Object executeWithResource(IRapidFireResource job) throws ExecutionException {
+    protected void performAction(IRapidFireJobResource job) throws Exception {
 
-        if (job instanceof IRapidFireJobResource) {
-            try {
+        try {
 
-                FileCopyStatusView view = (FileCopyStatusView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-                    .showView(FileCopyStatusView.ID);
-                view.setInput((IRapidFireJobResource)job);
+            FileCopyStatusView view = (FileCopyStatusView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+                .showView(FileCopyStatusView.ID);
+            view.setInput((IRapidFireJobResource)job);
 
-            } catch (PartInitException e) {
-                RapidFireCorePlugin.logError("*** Could not open job status view ***", e);
-                MessageDialog.openError(getShell(), Messages.E_R_R_O_R, ExceptionHelper.getLocalizedMessage(e));
-            } catch (Exception e) {
-                RapidFireCorePlugin.logError("*** Could load job statuses ***", e);
-                MessageDialog.openError(getShell(), Messages.E_R_R_O_R, ExceptionHelper.getLocalizedMessage(e));
-            }
+        } catch (PartInitException e) {
+            RapidFireCorePlugin.logError("*** Could not open job status view ***", e);
+            MessageDialog.openError(getShell(), Messages.E_R_R_O_R, ExceptionHelper.getLocalizedMessage(e));
+        } catch (Exception e) {
+            RapidFireCorePlugin.logError("*** Could load job statuses ***", e);
+            MessageDialog.openError(getShell(), Messages.E_R_R_O_R, ExceptionHelper.getLocalizedMessage(e));
         }
-
-        return null;
-    }
-
-    public void setEnabled(Object paramObject) {
-    }
-
-    public void setEnabledWDSCi(ISelection selection) {
     }
 }
