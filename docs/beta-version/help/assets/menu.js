cfa254f52c90c9abd5e184bd1e09822db68c49e0
Thomas Raddatz <thomas.raddatz@tools400.de>
2024-01-27 19:44:33 +0100

    Upload Rapid Fire update site (generated).

---
+++ docs/beta-version/help/assets/menu.js
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------- //
+//  http://www.dynamicdrive.com/dynamicindex1/navigate1.htm   //
+// ---------------------------------------------------------- //
+document.write('\<ul id="treemenu" class="treeview">\
+  <li><a href="../html/introduction/introduction.html" target="content" style="text-decoration:none">Introduction</a></li>\
+  <li><a href="../html/prerequisites/prerequisites.html" target="content" style="text-decoration:none">Prerequisites</a></li>\
+  <li>Install Instructions\
+  <ul>\
+    <li><a href="../html/install/install_library.html" target="content" style="text-decoration:none">Install Instructions</a></li>\
+  </ul>\
+  </li>\
+  <li><a href="../html/deploying_files_today/deploying_files_today.html" target="content" style="text-decoration:none">Deploying Data File Changes Today</a></li>\
+  <li><a href="../html/deploying_files_with_rapidfire/deploying_files_with_rapidfire.html" target="content" style="text-decoration:none">Deploying Data File Changes With Rapid Fire</a></li>\
+  <li><a href="../html/er_model/er_model.html" target="content" style="text-decoration:none">ER Model and Overview</a></li>\
+  <li><a href="../html/create_job/create_job.html" target="content" style="text-decoration:none">Creating a Rapid Fire Job</a></li>\
+  <li><a href="../html/add_file/add_file.html" target="content" style="text-decoration:none">Adding a File to a Rapid Fire Job</a></li>\
+  <li><a href="../html/menu_options/menu_options.html" target="content" style="text-decoration:none">Menu Options</a></li>\
+  <li>Preferences\
+  <ul>\
+    <li><a href="../html/preferences/appearance/appearance.html" target="content" style="text-decoration:none">Appearance</a></li>\
+    <li><a href="../html/preferences/library/library.html" target="content" style="text-decoration:none">Library</a></li>\
+  </ul>\
+  </li>\
+  <li>Uninstall Instructions\
+  <ul>\
+    <li><a href="../html/uninstall/uninstall_library.html" target="content" style="text-decoration:none">Uninstall Library</a></li>\
+  </ul>\
+  </li>\
+</ul>\
+');
